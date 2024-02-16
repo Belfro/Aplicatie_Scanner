c7bb18bcce05c6a3ac89b79143c4b7393fe6ab71
Andrei Stan <stanfandrei@yahoo.com>
2024-02-16 15:21:10 +0200

    Update IP New Server

--- Aplicatie_Scanner/obj/Release/net6.0-windows/win-x86/Aplicatie_Scanner.AssemblyInfo.cs
+++ Aplicatie_Scanner/obj/Release/net6.0-windows/win-x86/Aplicatie_Scanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a0453a16c084798873dbcf4e621e8cf1a18672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
