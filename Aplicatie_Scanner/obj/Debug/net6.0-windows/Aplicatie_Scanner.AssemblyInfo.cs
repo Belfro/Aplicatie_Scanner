0566303aee2162a66bf4cc6aaaeae2067fbc93c5
Andrei Stan <stanfandrei@yahoo.com>
2024-06-26 13:18:54 +0300

    a

--- Aplicatie_Scanner/obj/Debug/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
+++ Aplicatie_Scanner/obj/Debug/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ae535852e726e986c4598f747530e25ca69819")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
