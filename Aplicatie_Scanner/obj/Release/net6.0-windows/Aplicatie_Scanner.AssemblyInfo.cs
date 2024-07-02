264b74cd41440078227203f3e0eba7155cf97e88
Andrei Stan <stanfandrei@yahoo.com>
2024-07-02 13:53:11 +0300

    Update Raport Faptic

--- Aplicatie_Scanner/obj/Release/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
+++ Aplicatie_Scanner/obj/Release/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080625286de42fc12d76592738e32687aca3d2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cc66444d970005af81d1962a4392b59caabf9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
