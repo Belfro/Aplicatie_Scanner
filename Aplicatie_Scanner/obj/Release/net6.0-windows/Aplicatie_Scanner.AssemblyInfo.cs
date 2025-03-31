ef4079d6959f03e36675a53f75eaa76a4268823c
Andrei Stan <stanfandrei@yahoo.com>
2025-03-31 14:15:11 +0300

    Bugfix corectie depozit 1.0

--- Aplicatie_Scanner/obj/Release/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
+++ Aplicatie_Scanner/obj/Release/net6.0-windows/Aplicatie_Scanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70fa0d2e163e497658f41eb17bfcd5bd14f6392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9060750e4549b9e725371da06f5ff42445ad949")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicatie_Scanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
